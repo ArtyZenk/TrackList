64e8f3b18a75e848e23f25dcfa3d91ea86a4f22d
Artyom <artyomka13@gmail.com>
2022-06-10 14:29:30 +0300

    add content for model

--- TrackList/Track.swift
+++ TrackList/Track.swift
@@ -15,16 +15,16 @@ struct Track {
     
     static func getTrackList() -> [Track] {
         [
-            Track(artist: "1", song: "2"),
-            Track(artist: "1", song: "2"),
-            Track(artist: "1", song: "2"),
-            Track(artist: "1", song: "2"),
-            Track(artist: "1", song: "2"),
-            Track(artist: "1", song: "2"),
-            Track(artist: "1", song: "2"),
-            Track(artist: "1", song: "2"),
-            Track(artist: "1", song: "2"),
-            Track(artist: "1", song: "2")
+            Track(artist: "Korn", song: "All in the family"),
+            Track(artist: "Korn", song: "Blind"),
+            Track(artist: "Metallica", song: "Enter Sandman"),
+            Track(artist: "Metallica", song: "Kill them all"),
+            Track(artist: "Metallica", song: "Master Of Puppets"),
+            Track(artist: "Pantera", song: "Love"),
+            Track(artist: "Pantera", song: "Walk"),
+            Track(artist: "Rammstein", song: "Alexander"),
+            Track(artist: "Rammstein", song: "Deutschland"),
+            Track(artist: "Rammstein", song: "Herzeleid")
         ]
     }
 }
